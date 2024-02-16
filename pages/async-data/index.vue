0f7d23fa1955b7f96690575363c15e953e421bf8
mraju10 <mraju10@asu.edu>
2024-02-16 15:47:32 +0530

    cl/cd initial commit

---
+++ pages/async-data/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    ------------
+    {{ pageData.data }}
+  </div>
+</template>
+
+<script setup>
+const pageData = await useAsyncData("home", () =>
+  queryContent("home").findOne()
+);
+</script>
+
+<style lang="sass" scoped></style>
---
+++ pages/query-content/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ pageData[0] }}
+  </div>
+  <div>
+    <img
+      :src="`images/${pageData[0].sectionOnlineProgram.image}`"
+      :alt="pageData[0].sectionOnlineProgram.altText"
+      width="200px"
+      height="200px"
+    />
+    <h1>title----{{ pageData[0].sectionOnlineProgram.title }}</h1>
+    <p>text------{{ pageData[0].sectionOnlineProgram.text }}</p>
+    <div
+      v-for="(fact, index) in pageData[0].sectionOnlineProgram.facts"
+      :key="index"
+    >
+      <h1>array title--------{{ fact.title }}</h1>
+      <p>array text----------{{ fact.text }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+let pageData = await queryContent("home").find();
+</script>
+
+<style lang="sass" scoped></style>
